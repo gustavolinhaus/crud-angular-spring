fdfe558b29f6e35a581e2bf51da6ffdc32c4299b
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2023-04-03 14:16:03 -0300

    Criando um Service no Angular

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,14 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Course} from '../model/course';
+import {CoursesService} from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
-  courses: Course[] = [
-    { _id: "1", name: 'Angular', category: 'front-end' }
-  ];
+export class CoursesComponent implements OnInit {
+  courses: Course[] = [];
   displayedColumns = ['name', 'category'];
+
+  constructor(private coursesService: CoursesService) {
+  }
+
+  ngOnInit(): void {
+    this.courses = this.coursesService.list();
+  }
 }
