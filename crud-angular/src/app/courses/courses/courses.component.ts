bc146e945affdece1ce7206088d7a7bdf5c0e808
Gustavo Linhaus <gustavo_linhaus@hotmail.com>
2023-04-03 14:54:19 -0300

    Chamada HTTP Get no Angular e RXJS

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Observable} from 'rxjs';
 import {Course} from '../model/course';
 import {CoursesService} from '../services/courses.service';
 
@@ -8,13 +9,14 @@ import {CoursesService} from '../services/courses.service';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [];
+  courses: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
   constructor(private coursesService: CoursesService) {
+    this.courses = this.coursesService.list();
   }
 
   ngOnInit(): void {
-    this.courses = this.coursesService.list();
+
   }
 }
